9472472149a8cec2ab82c3ce492dba83597797e1
Katie Blizzard <kblizzard11@gmail.com>
2019-05-02 22:54:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,6 +47,21 @@ class Student
     student.grade = row[2]
     student
   end
+  
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM students WHERE name = ? LIMIT 1
+    SQL
+    
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first 
+    
+  end
+  
+  def update
+    
+  
 
 
 end
