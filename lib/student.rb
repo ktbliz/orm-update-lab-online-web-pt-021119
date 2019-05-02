bd776462d544c293eca487d0f10af35b740538e1
Katie Blizzard <kblizzard11@gmail.com>
2019-05-02 22:55:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,7 +60,9 @@ class Student
   end
   
   def update
-    
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end 
   
 
 
