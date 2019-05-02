92ccfe91642e5c9ef902da143f1cd95a94fa6e28
Katie Blizzard <kblizzard11@gmail.com>
2019-05-02 22:53:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,6 +40,13 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  def self.new_from_db(row)
+    student = self.new 
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
+  end
 
 
 end
